008bd2bb7dfa05c54866ee838d2bbe7db8568b3c
kotbegemot <k0tb9g9m0t@yandex.ru>
2017-09-13 11:13:14 +0300

    more multiple changes

--- include/appbase/application.hpp
+++ include/appbase/application.hpp
@@ -3,6 +3,7 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/core/demangle.hpp>
 #include <boost/asio.hpp>
+#include <boost/throw_exception.hpp>
 
 #include <iostream>
 
@@ -10,159 +11,159 @@
 
 namespace appbase {
 
-   namespace bpo = boost::program_options;
-   namespace bfs = boost::filesystem;
+    namespace bpo = boost::program_options;
+    namespace bfs = boost::filesystem;
 
-   class application {
-      public:
-         ~application();
+    class application {
+    public:
+        ~application();
 
-         /**
-          * @brief Looks for the --plugin commandline / config option and calls initialize on those plugins
-          *
-          * @tparam Plugin List of plugins to initalize even if not mentioned by configuration. For plugins started by
-          * configuration settings or dependency resolution, this template has no effect.
-          * @return true if the application and plugins were initialized, false or exception on error
-          */
-         template< typename... Plugin >
-         bool initialize( int argc, char** argv ) {
-            return initialize_impl( argc, argv, { find_plugin< Plugin >()... } );
-         }
+        /**
+         * @brief Looks for the --plugin commandline / config option and calls initialize on those plugins
+         *
+         * @tparam Plugin List of plugins to initalize even if not mentioned by configuration. For plugins started by
+         * configuration settings or dependency resolution, this template has no effect.
+         * @return true if the application and plugins were initialized, false or exception on error
+         */
+        template< typename... Plugin >
+        bool initialize( int argc, char** argv ) {
+            return initialize_impl( argc, argv, { find_plugin( Plugin::name() )... } );
+        }
 
-         void startup();
-         void shutdown();
+        void startup();
+        void shutdown();
 
-         /**
-          *  Wait until quit(), SIGINT or SIGTERM and then shutdown
-          */
-         void exec();
-         void quit();
+        /**
+         *  Wait until quit(), SIGINT or SIGTERM and then shutdown
+         */
+        void exec();
+        void quit();
 
-         static application& instance( bool reset = false );
+        static application& instance( bool reset = false );
 
-         abstract_plugin* find_plugin( const string& name )const;
-         abstract_plugin& get_plugin( const string& name )const;
-
-         template< typename Plugin >
-         Plugin& register_plugin() {
-            auto existing = find_plugin< Plugin >();
+        template< typename Plugin >
+        Plugin& register_plugin() {
+            auto existing = find_plugin( Plugin::name() );
             if( existing )
-               return *existing;
+                return *dynamic_cast< Plugin* >( existing );
 
-            auto plug = new Plugin();
-            plugins[plug->name()].reset( plug );
+            auto plug = std::make_shared< Plugin >();
+            plugins[Plugin::name()] = plug;
             plug->register_dependencies();
             return *plug;
-         }
+        }
 
-         template< typename Plugin >
-         Plugin* find_plugin()const {
-            return dynamic_cast< Plugin* >( find_plugin( Plugin().name() ) );
-         }
+        template< typename Plugin >
+        Plugin* find_plugin()const {
+            Plugin* plugin = dynamic_cast< Plugin* >( find_plugin( Plugin::name() ) );
 
-         template< typename Plugin >
-         Plugin& get_plugin()const {
+            // Do not return plugins that are registered but not at least initialized.
+            if( plugin != nullptr && plugin->get_state() == abstract_plugin::registered )
+            {
+                return nullptr;
+            }
+
+            return plugin;
+        }
+
+        template< typename Plugin >
+        Plugin& get_plugin()const {
             auto ptr = find_plugin< Plugin >();
-             if( ptr == nullptr ) {
-                 BOOST_THROW_EXCEPTION( std::runtime_error( "unable to find plugin: " /*+ Plugin::name()*/ ) );
-             }
+            if( ptr == nullptr )
+                BOOST_THROW_EXCEPTION( std::runtime_error( "unable to find plugin: " + Plugin::name() ) );
             return *ptr;
-         }
+        }
 
-         bfs::path data_dir()const;
+        bfs::path data_dir()const;
 
-         void add_program_options( const bpo::options_description& cli, const bpo::options_description& cfg );
-         const bpo::variables_map& get_args() const;
+        void add_program_options( const bpo::options_description& cli, const bpo::options_description& cfg );
+        const bpo::variables_map& get_args() const;
 
-         void set_version_string( const string& version ) {
-             version_info = version;
-         }
+        void set_version_string( const string& version ) { version_info = version; }
 
-         boost::asio::io_service& get_io_service() {
-             return *io_serv;
-         }
+        boost::asio::io_service& get_io_service() { return *io_serv; }
 
-      protected:
-         template< typename Impl >
-         friend class plugin;
+    protected:
+        template< typename Impl >
+        friend class plugin;
 
-         bool initialize_impl( int argc, char** argv, vector< abstract_plugin* > autostart_plugins );
+        bool initialize_impl( int argc, char** argv, vector< abstract_plugin* > autostart_plugins );
 
-         /** these notifications get called from the plugin when their state changes so that
-          * the application can call shutdown in the reverse order.
-          */
-         ///@{
-         void plugin_initialized( abstract_plugin& plug ) { initialized_plugins.push_back( &plug ); }
-         void plugin_started( abstract_plugin& plug ) { running_plugins.push_back( &plug ); }
-         ///@}
+        abstract_plugin* find_plugin( const string& name )const;
+        abstract_plugin& get_plugin( const string& name )const;
 
-      private:
-         application(); ///< private because application is a singlton that should be accessed via instance()
-         map< string, std::shared_ptr< abstract_plugin > >  plugins; ///< all registered plugins
-         vector< abstract_plugin* >                         initialized_plugins; ///< stored in the order they were started running
-         vector< abstract_plugin* >                         running_plugins; ///< stored in the order they were started running
-         std::shared_ptr< boost::asio::io_service >         io_serv;
-         std::string                                        version_info;
+        /** these notifications get called from the plugin when their state changes so that
+         * the application can call shutdown in the reverse order.
+         */
+        ///@{
+        void plugin_initialized( abstract_plugin& plug ) { initialized_plugins.push_back( &plug ); }
+        void plugin_started( abstract_plugin& plug ) { running_plugins.push_back( &plug ); }
+        ///@}
 
-         void set_program_options();
-         void write_default_config( const bfs::path& cfg_file );
-         std::unique_ptr< class application_impl > my;
+    private:
+        application(); ///< private because application is a singlton that should be accessed via instance()
+        map< string, std::shared_ptr< abstract_plugin > >  plugins; ///< all registered plugins
+        vector< abstract_plugin* >                         initialized_plugins; ///< stored in the order they were started running
+        vector< abstract_plugin* >                         running_plugins; ///< stored in the order they were started running
+        std::shared_ptr< boost::asio::io_service >         io_serv;
+        std::string                                        version_info;
 
-   };
+        void set_program_options();
+        void write_default_config( const bfs::path& cfg_file );
+        std::unique_ptr< class application_impl > my;
 
-   application& app();
-   application& reset();
+    };
 
-   template< typename Impl >
-   class plugin : public abstract_plugin {
-      public:
-         plugin() : _name( boost::core::demangle( typeid( Impl ).name() ) ) {}
-         virtual ~plugin() {}
+    application& app();
+    application& reset();
 
-         virtual state get_state()const override         { return _state; }
-         virtual const std::string& name() const override { return _name; }
+    template< typename Impl >
+    class plugin : public abstract_plugin {
+    public:
+        virtual ~plugin() {}
 
-         virtual void register_dependencies() {
-            static_cast< Impl* >( this )->plugin_requires( [&]( abstract_plugin& plug ){} );
-         }
+        virtual state get_state() const override { return _state; }
+        virtual const std::string& get_name()const override final { return Impl::name(); }
 
-         virtual void initialize(const variables_map& options) override {
+        virtual void register_dependencies() {
+            this->plugin_for_each_dependency( [&]( abstract_plugin& plug ){} );
+        }
+
+        virtual void initialize(const variables_map& options) override final {
             if( _state == registered ) {
-               _state = initialized;
-               static_cast< Impl* >( this )->plugin_requires( [&]( abstract_plugin& plug ){ plug.initialize( options ); } );
-               static_cast< Impl* >( this )->plugin_initialize( options );
-               // std::cout << "initializing plugin " << name() << std::endl;
-               app().plugin_initialized( *this );
+                _state = initialized;
+                this->plugin_for_each_dependency( [&]( abstract_plugin& plug ){ plug.initialize( options ); } );
+                this->plugin_initialize( options );
+                // std::cout << "Initializing plugin " << Impl::name() << std::endl;
+                app().plugin_initialized( *this );
             }
-            assert( _state == initialized ); /// if initial state was not registered, final state cannot be initiaized
-         }
+            if (_state != initialized)
+                BOOST_THROW_EXCEPTION( std::runtime_error("Initial state was not registered, so final state cannot be initialized.") );
+        }
 
-         virtual void startup() override {
+        virtual void startup() override final {
             if( _state == initialized ) {
-               _state = started;
-               static_cast< Impl* >( this )->plugin_requires( [&]( abstract_plugin& plug ){ plug.startup(); } );
-               static_cast< Impl* >( this )->plugin_startup();
-               app().plugin_started( *this );
+                _state = started;
+                this->plugin_for_each_dependency( [&]( abstract_plugin& plug ){ plug.startup(); } );
+                this->plugin_startup();
+                app().plugin_started( *this );
             }
-            assert( _state == started ); // if initial state was not initialized, final state cannot be started
-         }
+            if (_state != started )
+                BOOST_THROW_EXCEPTION( std::runtime_error("Initial state was not initialized, so final state cannot be started.") );
+        }
 
-         virtual void shutdown() override {
+        virtual void shutdown() override final {
             if( _state == started ) {
-               _state = stopped;
-               //ilog( "shutting down plugin ${name}", ("name",name()) );
-               static_cast< Impl* >( this )->plugin_shutdown();
+                _state = stopped;
+                //ilog( "shutting down plugin ${name}", ("name",name()) );
+                this->plugin_shutdown();
             }
-         }
+        }
 
-      protected:
-         plugin( const string& name ) : _name( name ) {}
-       void name(const std::string& name){
-           _name=name;
-       }
+    protected:
+        plugin() = default;
 
-      private:
-         state _state = abstract_plugin::registered;
-         std::string _name;
-   };
+    private:
+        state _state = abstract_plugin::registered;
+    };
 }
